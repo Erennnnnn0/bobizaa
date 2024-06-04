0d7f85b814be8689042d4764fe651ce5ac6fe9c3
Erenyeager1111 <brahimyeager123@gmail.com>
2024-06-04 06:10:22 +0100

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, participants }) => {
             const res = await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
             await delay(1 * 1000)
         }
-    m.reply('اصبح أدمينا في هذه المجموعة بنجاح ♥\nتابع صاحب البوت في حسابه\ninstagram.com/noureddine_ouafy')
+    m.reply('مبروك صرت مشرف 🎉\')
 
 }
 handler.help = ['admin']
