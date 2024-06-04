e1511d153c4f493cf1e2390b6305d61c9f52425f
Erenyeager1111 <brahimyeager123@gmail.com>
2024-06-05 00:05:11 +0100

    Create Erenpin.js

---
+++ Erenpin.js
@@ -0,0 +1,35 @@
+import pywhatkit as kit
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def download_pinterest_video(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    video_tag = soup.find('video')
+
+    if video_tag and video_tag['src']:
+        video_url = video_tag['src']
+        video_response = requests.get(video_url)
+
+        file_name = "pinterest_video.mp4"
+        with open(file_name, 'wb') as video_file:
+            video_file.write(video_response.content)
+        return file_name
+    else:
+        return None
+
+def send_video_whatsapp(phone_number, video_path, message):
+    kit.sendwhats_image(phone_number, video_path, caption=message)
+
+# مثال على الاستخدام:
+pinterest_video_url = "رابط الفيديو من بينتريست هنا"
+phone_number = "رقم الهاتف بصيغة دولية مع رمز الدولة"
+message = "شاهد هذا الفيديو من بينتريست!"
+
+video_path = download_pinterest_video(pinterest_video_url)
+
+if video_path:
+    send_video_whatsapp(phone_number, video_path, message)
+else:
+    print("لم أتمكن من تحميل الفيديو من بينتريست.")
