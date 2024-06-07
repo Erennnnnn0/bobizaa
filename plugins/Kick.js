7261d7c929a3471ec01537570e47da97f9bc559d
Erenyeager1111 <brahimyeager123@gmail.com>
2024-06-07 14:54:26 +0100

    Update Kick.js

--- plugins/Kick.js
+++ plugins/Kick.js
@@ -1,17 +1,4 @@
 import { areJidsSameUser } from "@adiwajshing/baileys";
-let handler = async (m, { conn, participants, isOwner }) => {
-  let usr = m.quoted ? [m.quoted.sender] : m.mentionedJid;
-  let users = usr.filter((u) => !areJidsSameUser(u, conn.user.id));
-  if (!(m.quoted || m.mentionedJid[0]))
-    return m.reply("Tag atau reply orang yang mau dikick!");
-  let kickedUser = [];
-  for (let user of users)
-    if (
-      user.endsWith("@s.whatsapp.net") &&
-      !(
-        participants.find((v) => areJidsSameUser(v.id, user)) || { admin: true }
-      ).admin
-    ) {
       const res = await conn.groupParticipantsUpdate(m.chat, [user], "remove");
       kickedUser.concat(res);
       await delay(1 * 1000);
