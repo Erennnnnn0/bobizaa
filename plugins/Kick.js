1a08bf9db7653dc1d9089d168f6ac88d05bf29b8
Erenyeager1111 <brahimyeager123@gmail.com>
2024-06-08 06:29:37 +0100

    Update Kick.js

--- plugins/Kick.js
+++ plugins/Kick.js
@@ -1,32 +1,20 @@
-import { areJidsSameUser } from "@adiwajshing/baileys";
-let handler = async (m, { conn, participants, isOwner }) => {
-  let usr = m.quoted ? [m.quoted.sender] : m.mentionedJid;
-  let users = usr.filter((u) => !areJidsSameUser(u, conn.user.id));
-  if (!(m.quoted || m.mentionedJid[0]))
-    return m.reply("Tag atau reply orang yang mau dikick!");
-  let kickedUser = [];
-  for (let user of users)
-    if (
-      user.endsWith("@s.whatsapp.net") &&
-      !(
-        participants.find((v) => areJidsSameUser(v.id, user)) || { admin: true }
-      ).admin
-      const handler = async (m, {conn, participants, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_kick2
+let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-  if (!global.db.data.settings[conn.user.jid].restrict) throw`${tradutor.texto1[0]} (𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝 / 𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) ${tradutor.texto1[1]}`;
-  const kicktext = `${tradutor.texto2} \n*${usedPrefix + command} @${global.suittag}*`;
-  if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
-  if (m.mentionedJid.includes(conn.user.jid)) return;
-  const user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-  const owr = m.chat.split`-`[0];
-  await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
-};
-handler.command = /^(kick2|echar2|hechar2|sacar2)$/i;
-handler.admin = true;
-handler.group = true;
-handler.botAdmin = true;
-export default handler;
+let kickte = `*وين شخص الي راح ينشات!*`
+
+if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+let owr = m.chat.split`-`[0]
+await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+m.reply(`*طردك فحلك روح ابكي عند امك ترجعك 😲!*`) 
+
+}
+
+handler.help = ['kick @user']
+handler.tags = ['group']
+handler.command = ['kick', 'شوت'] 
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler
