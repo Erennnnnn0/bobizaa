99e3860e967ac1818256a2004f14f2e2a32177ae
Erenyeager1111 <brahimyeager123@gmail.com>
2024-06-08 02:38:19 +0100

    Update Kick.js

--- plugins/Kick.js
+++ plugins/Kick.js
@@ -1,4 +1,17 @@
-const handler = async (m, {conn, participants, usedPrefix, command}) => {
+import { areJidsSameUser } from "@adiwajshing/baileys";
+let handler = async (m, { conn, participants, isOwner }) => {
+  let usr = m.quoted ? [m.quoted.sender] : m.mentionedJid;
+  let users = usr.filter((u) => !areJidsSameUser(u, conn.user.id));
+  if (!(m.quoted || m.mentionedJid[0]))
+    return m.reply("Tag atau reply orang yang mau dikick!");
+  let kickedUser = [];
+  for (let user of users)
+    if (
+      user.endsWith("@s.whatsapp.net") &&
+      !(
+        participants.find((v) => areJidsSameUser(v.id, user)) || { admin: true }
+      ).admin
+      const handler = async (m, {conn, participants, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
