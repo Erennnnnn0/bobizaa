1d1a6729251da51f341f5f64a647fd08d145e0a5
Erenyeager1111 <brahimyeager123@gmail.com>
2024-06-08 02:32:22 +0100

    Update Kick.js

--- plugins/Kick.js
+++ plugins/Kick.js
@@ -1,58 +1,19 @@
-import { areJidsSameUser } from "@adiwajshing/baileys";
-let handler = async (m, { conn, participants, isOwner }) => {
-  let usr = m.quoted ? [m.quoted.sender] : m.mentionedJid;
-  let users = usr.filter((u) => !areJidsSameUser(u, conn.user.id));
-  if (!(m.quoted || m.mentionedJid[0]))
-    return m.reply("Tag atau reply orang yang mau dikick!");
-  let kickedUser = [];
-  for (let user of users)
-    if (
-      user.endsWith("@s.whatsapp.net") &&
-      !(
-        participants.find((v) => areJidsSameUser(v.id, user)) || { admin: true }
-      ).admin
-    ) {
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const handler = async (m, {conn, participants, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.gc_kick2
 
-const client = new Client({
-    authStrategy: new LocalAuth()
-});
-
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', async msg => {
-    if (msg.body.startsWith('!kick ')) {
-        const mentioned = msg.mentionedIds[0];
-        
-        if (msg.from.includes('@g.us')) {
-            const chat = await msg.getChat();
-
-            if (chat.isGroup) {
-                const participants = await chat.participants;
-                const sender = participants.find(p => p.id._serialized === msg.author);
-
-                if (sender.isAdmin) {
-                    const user = participants.find(p => p.id._serialized === mentioned);
-                    if (user) {
-                        await chat.removeParticipants([mentioned]);
-                        msg.reply(`User ${mentioned} has been removed from the group.`);
-                    } else {
-                        msg.reply('User is not in the group.');
-                    }
-                } else {
-                    msg.reply('You need to be an admin to kick members.');
-                }
-            }
-        } else {
-            msg.reply('This command can only be used in groups.');
-        }
-    }
-});
-
-client.initialize();
+  if (!global.db.data.settings[conn.user.jid].restrict) throw`${tradutor.texto1[0]} (𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝 / 𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) ${tradutor.texto1[1]}`;
+  const kicktext = `${tradutor.texto2} \n*${usedPrefix + command} @${global.suittag}*`;
+  if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
+  if (m.mentionedJid.includes(conn.user.jid)) return;
+  const user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+  const owr = m.chat.split`-`[0];
+  await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+};
+handler.command = /^(kick2|echar2|hechar2|sacar2)$/i;
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
